bfbaae08340982014a62dcacc8fabf9aac52ffe2
nikhilbabu007 <nikhilvanajababu@gmail.com>
2026-08-11 21:47:41 +0530

    Remove sensitive credentials from settings
    Clear sensitive information from email and Razorpay settings.

--- main_pro/settings.py
+++ main_pro/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nikksnikhil07@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD =''
 
-RAZORPAY_KEY_ID = 'YOUR_KEY_ID'
-RAZORPAY_KEY_SECRET = 'YOUR_KEY_SECRET'
+RAZORPAY_KEY_ID = ''
+RAZORPAY_KEY_SECRET = ''
